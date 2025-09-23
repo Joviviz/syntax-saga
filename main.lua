b4cac735b2dd0ae390773c52b26c30fa852406f6
Joviviz <101208535+Joviviz@users.noreply.github.com>
2025-09-23 17:07:23 -0300

    Create first movement system + add gitignore

--- main.lua
+++ main.lua
@@ -1,3 +1,25 @@
+function love.load()
+	player = {}
+	player.x = 100
+	player.y = 100
+	player.speed = 3
+end
+
+function love.update(dt)
+	if love.keyboard.isDown("right") then
+		player.x = player.x + player.speed
+	end
+	if love.keyboard.isDown("left") then
+		player.x = player.x - player.speed
+	end
+	if love.keyboard.isDown("down") then
+		player.x = player.y - player.speed
+	end
+	if love.keyboard.isDown("up") then
+		player.x = player.y + player.speed
+	end
+end
+
 function love.draw()
-	love.graphics.print("Hello, Syntax Saga!", 400, 300)
+	love.graphics.circle("fill", player.x, player.y, 100)
 end
